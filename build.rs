f6baca27a1796ada56c211aab74bf93c1d38d1e3
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-07-30 12:55:56 +0200

    get version for docker variable

--- build.rs
+++ build.rs
@@ -1,13 +1,20 @@
 
+use std::env;
 use std::process::Command;
 
 fn main() {
-  let cmd = Command::new("git")
-    .args(&["describe", "--tag", "--always"])
-    .output()
-    .unwrap();
-  assert!(cmd.status.success());
-  let ver = std::str::from_utf8(&cmd.stdout[..]).unwrap().trim();
-  println!("cargo:rustc-env={}={}", "VERSION", ver);
+  let version = 
+    if let Ok(version) = env::var("IMAGE_TAG") {
+      version
+    } else {
+      let cmd = Command::new("git")
+        .args(&["describe", "--tag", "--always"])
+        .output()
+        .unwrap();
+      assert!(cmd.status.success());
+      std::str::from_utf8(&cmd.stdout[..]).unwrap().trim().to_string()
+    };
+
+  println!("cargo:rustc-env={}={}", "VERSION", version);
   println!("cargo:rerun-if-changed=(nonexistentfile)");
 }
