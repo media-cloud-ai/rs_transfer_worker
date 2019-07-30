0f2c2ad4cbc94f7bdc7cfa966f5d982c7f0893fd
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-07-30 11:48:00 +0200

    add build.rs

---
+++ build.rs
@@ -0,0 +1,13 @@
+
+use std::process::Command;
+
+fn main() {
+  let cmd = Command::new("git")
+    .args(&["describe", "--tag", "--always"])
+    .output()
+    .unwrap();
+  assert!(cmd.status.success());
+  let ver = std::str::from_utf8(&cmd.stdout[..]).unwrap().trim();
+  println!("cargo:rustc-env={}={}", "VERSION", ver);
+  println!("cargo:rerun-if-changed=(nonexistentfile)");
+}
