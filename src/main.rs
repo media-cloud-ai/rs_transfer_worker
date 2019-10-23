e83262f8e667aa44c36438fd7b1cb07b9e56f272
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-10-23 20:09:08 +0200

    update rs_amqp_worker to 0.6.0

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,185 @@ extern crate reqwest;
 extern crate rusoto_core;
 extern crate rusoto_credential;
 extern crate rusoto_s3;
+extern crate semver;
 extern crate tokio;
 extern crate tokio_io;
 extern crate url;
 
 use amqp_worker::*;
+use semver::Version;
 
 mod message;
 mod reader;
 mod target_configuration;
 mod writer;
 
-#[derive(Debug)]
-struct FtpEvent {}
+use amqp_worker::worker::{Parameter, ParameterType};
 
-impl MessageEvent for FtpEvent {
+macro_rules! crate_version {
+    () => {
+        env!("CARGO_PKG_VERSION")
+    };
+}
+
+#[derive(Debug)]
+struct TransferEvent {}
+
+impl MessageEvent for TransferEvent {
+  fn get_name(&self) -> String {
+    "Transfer".to_string()
+  }
+
+  fn get_short_description(&self) -> String {
+    "Move file from any storage".to_string()
+  }
+
+  fn get_description(&self) -> String {
+    r#"Move any file from a location to an another one else.
+It support in input: Local, FTP, S3, HTTP.
+It support in output: Local, FTP, S3."#.to_string()
+  }
+
+  fn get_version(&self) -> Version {
+    semver::Version::parse(crate_version!()).expect("unable to locate Package version")
+  }
+
+  fn get_git_version(&self) -> Version {
+    semver::Version::parse(crate_version!()).expect("unable to locate Package version")
+  }
+
+  fn get_parameters(&self) -> Vec<Parameter> {
+    vec![
+      Parameter {
+        identifier: "source_path".to_string(),
+        label: "Source path".to_string(),
+        kind: vec![ParameterType::String],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_hostname".to_string(),
+        label: "Source hostname".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_username".to_string(),
+        label: "Source username".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_password".to_string(),
+        label: "Source password".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_access_key".to_string(),
+        label: "Source access key".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_secret_key".to_string(),
+        label: "Source secret key".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_region".to_string(),
+        label: "Source region".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_prefix".to_string(),
+        label: "Source prefix".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_port".to_string(),
+        label: "Source port".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "source_ssl".to_string(),
+        label: "Source ssl".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_path".to_string(),
+        label: "Source path".to_string(),
+        kind: vec![ParameterType::String],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_hostname".to_string(),
+        label: "Destination hostname".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_username".to_string(),
+        label: "Destination username".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_password".to_string(),
+        label: "Destination password".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_access_key".to_string(),
+        label: "Destination access key".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_secret_key".to_string(),
+        label: "Destination secret key".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_region".to_string(),
+        label: "Destination region".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_prefix".to_string(),
+        label: "Destination prefix".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_port".to_string(),
+        label: "Destination port".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      },
+      Parameter {
+        identifier: "destination_ssl".to_string(),
+        label: "Destination ssl".to_string(),
+        kind: vec![ParameterType::Credential],
+        required: true,
+      }
+    ]
+  }
+
   fn process(&self, msg: &str) -> Result<job::JobResult, MessageError> {
     message::process(msg)
   }
 }
 
-static FTP_EVENT: FtpEvent = FtpEvent {};
+static TRANSFER_EVENT: TransferEvent = TransferEvent {};
 
 fn main() {
-  start_worker(&FTP_EVENT);
+  start_worker(&TRANSFER_EVENT);
 }
